87f96a1bbad2c1c167125a77052ad7d3a125e19a
Kevin Mas <kmas@thess-corp.fr>
2023-08-20 08:19:18 +0200

    feat: adding trends and projections

--- components/trendsAndProjections/trendsAndProjections.tsx
+++ components/trendsAndProjections/trendsAndProjections.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import trendsAndProjectionDataElements from "../../data/currentAndFuturetrendsData.json";
 import "./trendsAndProjections.scss";
 
@@ -6,9 +8,12 @@ export type trendsAndProjectionElementType = {
 	title: string;
 	current: string;
 	future: string;
+	image: string;
 };
 
 export default function TrendsAndProjections() {
+	const [isCurrentTrend, setIsCurrentTrend] = useState(true);
+
 	return (
 		<section className="trends">
 			<header className="trends__header">
@@ -20,11 +25,29 @@ export default function TrendsAndProjections() {
 					current trends and future projections in web development
 				</p>
 				<div className="separator" />
+				<p className="webApps__header--info">
+					Hover or touch each cell to discover the current trend and click again to discover future projections
+				</p>
 			</header>
-			<div className="trends__content">
+			<div className="trends__content honeycomb">
 				{trendsAndProjectionDataElements.map((element) => {
-					return <div key={element.id}>TEST</div>;
+					return (
+						<li className="honeycomb-cell" key={element.id}>
+							<img className="honeycomb-cell__image" src={element.image} alt={element.title} />
+							<div className="honeycomb-cell__title">{element.title}</div>
+							<div className="honeycomb-cell__descriptionCurrent">
+								<div
+									className="honeycomb-cell__descriptionCurrent--content subtitle"
+									onClick={() => setIsCurrentTrend(!isCurrentTrend)}
+								>
+									<div>{isCurrentTrend ? "Current Trend:" : "Future Projection:"}</div>
+									{isCurrentTrend ? element.current : element.future}
+								</div>
+							</div>
+						</li>
+					);
 				})}
+				<li className="honeycomb-cell honeycomb__placeholder"></li>
 			</div>
 		</section>
 	);
